bbff755e1340542280080ed20189a28304fa78a6
Mlkz <maho.mamulashvili@gmail.com>
2022-12-23 10:54:38 -0500

    add migration, user model, user contorller

---
+++ RedditOnRails/app/models/user.rb
@@ -0,0 +1,43 @@
+class User < ApplicationRecord
+  validates :username, :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: :true
+  validates :password, length: { minimum: 6 }, allow_nil: true
+
+  before_validation :ensure_session_token
+
+  attr_reader :password
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+
+    if user && user.is_password?(password)
+      return user
+    else
+      nill
+    end
+  end
+
+  def ensure_session_token
+    self.session_token ||= generate_session_token
+  end
+
+  def generate_session_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def reset_session_token!
+    self.session_token = generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  def is_password?(pass)
+    password_obj = BCrypt::Password.new(self.password_digest)
+    password_obj.is_password?(pass)
+  end
+
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+    @password = password
+  end
+end
