d16673ade3d6fdd0d3e818463340e7d9bbf4ffef
Mlkz <maho.mamulashvili@gmail.com>
2022-12-23 11:41:47 -0500

    Add application controller

--- RedditOnRails/app/controllers/application_controller.rb
+++ RedditOnRails/app/controllers/application_controller.rb
@@ -1,3 +1,26 @@
 class ApplicationController < ActionController::Base
-    
+  helper_method :logged_in?, :current_user
+
+  def current_user
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def login(user)
+    session[:session_token] = user.reset_session_token!
+  end
+
+  def logout!
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def require_logged_in
+    redirect_to new_session_url unless logged_in?
+  end
+
+  def require_logged_out
+    redirect_to new_session_url if logged_in?
+  end
 end
--- RedditOnRails/app/controllers/sessions_controller.rb
+++ RedditOnRails/app/controllers/sessions_controller.rb
@@ -1,30 +1,27 @@
 class SessionsController < ApplicationController
-    before_action :require_logged_in, only: [:new, :create]
-    before_action :require_logged_out, only: [:index, :edit, :update, :show]
+  before_action :require_logged_in, only: %i[destroy]
+  before_action :require_logged_out, only: %i[new create]
 
+  def new
+    @user = User.new
+    render :new
+  end
 
-    def new 
-        @user = User.new 
-        render :new
+  def create
+    username = params[:user][:username]
+    password = params[:user][:password]
+    @user = User.find_by_credentials(username, password)
+    if @user
+      login(@user)
+      redirect_to root #some index page
+    else
+      flash.now[:errors] = @user.erorrs.full_messages
+      render :new
     end
+  end
 
-
-    def create
-        username = params[:user][:username]
-        password = params[:user][:password]
-        @user = User.find_by_credentials(username, password)
-        if @user 
-            login(@user)
-            redirect_to root #some index page
-        else  
-            flash.now[:errors] = @user.erorrs.full_messages
-            render :new
-        end
-    end
-
-
-    def destroy
-        logout!
-        redirect_to new_session_url
-    end
+  def destroy
+    logout!
+    redirect_to new_session_url
+  end
 end
