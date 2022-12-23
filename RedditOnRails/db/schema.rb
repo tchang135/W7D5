ca8944c730afc8bcacf25540b753c016fc39e7c8
Timothy Chang <tchang135@gmail.com>
2022-12-23 11:57:07 -0500

    finished migration for sub table

--- RedditOnRails/db/schema.rb
+++ RedditOnRails/db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_23_153653) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_23_164527) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "subs", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "description"
+    t.bigint "moderator_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["moderator_id"], name: "index_subs_on_moderator_id"
+    t.index ["title", "moderator_id"], name: "index_subs_on_title_and_moderator_id", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
@@ -23,4 +33,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_23_153653) do
     t.index ["session_token"], name: "index_users_on_session_token"
   end
 
+  add_foreign_key "subs", "users", column: "moderator_id"
 end
