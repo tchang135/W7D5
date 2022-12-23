43ac36661fa98db16f5024b364b15e3785f90478
Timothy Chang <tchang135@gmail.com>
2022-12-23 11:13:54 -0500

    finished session controller

---
+++ RedditOnRails/app/controllers/sessions_controller.rb
@@ -0,0 +1,30 @@
+class SessionsController < ApplicationController
+    before_action :require_logged_in, only: [:new, :create]
+    before_action :require_logged_out, only: [:index, :edit, :update, :show]
+
+
+    def new 
+        @user = User.new 
+        render :new
+    end
+
+
+    def create
+        username = params[:user][:username]
+        password = params[:user][:password]
+        @user = User.find_by_credentials(username, password)
+        if @user 
+            login(@user)
+            redirect_to root #some index page
+        else  
+            flash.now[:errors] = @user.erorrs.full_messages
+            render :new
+        end
+    end
+
+
+    def destroy
+        logout!
+        redirect_to new_session_url
+    end
+end
