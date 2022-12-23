2ed6a6ad50ca3f5aecda2f03ced818251f7d35b3
Timothy Chang <tchang135@gmail.com>
2022-12-23 14:02:51 -0500

    writing subs controller up to edit action

--- RedditOnRails/app/controllers/subs_controller.rb
+++ RedditOnRails/app/controllers/subs_controller.rb
@@ -1,2 +1,56 @@
 class SubsController < ApplicationController
+
+    def index 
+        @subs = Sub.all 
+        render :index 
+    end
+
+
+    def new 
+        @sub = Sub.new 
+        render :new
+    end
+
+
+    def create
+        @sub = Sub.new(subs_params)
+        if @sub && @sub.save 
+            redirect_to sub_url(@sub)
+        else  
+            flash.now[:errors] = @sub.errors.full_messages
+            render :new
+        end
+    end
+
+
+    def show 
+        @sub = Sub.find_by(id: params[:id])
+        if @sub 
+            render :show
+        else  
+            flash[:errors] = @sub.errors.full_messages
+            redirect_to subs_url
+        end
+    end
+
+
+    def edit 
+        @sub = Sub.find_by(id: params[:id])
+        if @sub 
+            render :edit 
+        else  
+            flash.now[:errors] = @sub.errors.full_messages
+            render :show
+        end
+    end
+
+
+    def update
+
+    end
+
+
+    def subs_params 
+        params.require(:subs).permit(:title, :description, :moderator_id)
+    end
 end
